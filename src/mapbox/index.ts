cee973ce6dbc37e76bc0d02f7b1b101d8edfa8a1
aparrett <anthonyparrett7@gmail.com>
2024-04-26 14:24:22 -0500

    update MapboxFeature

--- src/mapbox/index.ts
+++ src/mapbox/index.ts
@@ -3,6 +3,7 @@ import { Coordinates } from '../utils';
 /** to see the full list of properties visit https://docs.mapbox.com/api/search/search-box/#search-request */
 export interface MapboxFeature {
   properties: {
+    name: string;
     place_formatted: string;
     coordinates: Coordinates;
   };
